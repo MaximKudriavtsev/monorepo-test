485b80e3cc5fdf891fec71673060a9749b232b10
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-11-27 19:41:35 +0300

    Remove excess files

--- packages/first-package/dist/first-package.es.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react';
-
-var FirstComponent = function FirstComponent(props) {
-  return createElement("div", props);
-};
-
-export { FirstComponent };
-//# sourceMappingURL=first-package.es.js.map
---
+++ packages/first-package/dist/first-package.umd.js
@@ -0,0 +1,16 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'react'], factory) :
+	(factory((global['first-package'] = {}),global.React));
+}(this, (function (exports,React) { 'use strict';
+
+	var FirstComponent = function FirstComponent(props) {
+	  return React.createElement("div", props);
+	};
+
+	exports.FirstComponent = FirstComponent;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=first-package.umd.js.map
