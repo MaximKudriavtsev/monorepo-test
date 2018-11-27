c93614e9b7eb7279a25d458efc027fa127f2f555
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-11-27 19:39:52 +0300

    Remove excess files

--- packages/first-package/rollup.config.js
+++ packages/first-package/rollup.config.js
@@ -1,14 +1,13 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import { banner, external, babelrc, globals, stubProcess } from '../../rollup-utils';
+import { external, babelrc } from '../../rollup-utils';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: "dist/first-package.umd.js", format: 'umd', name: 'first-package', sourcemap: true, globals: globals(), intro: stubProcess },
-    { file: "dist/first-package.es.js", format: 'es', sourcemap: true },
+    { file: "dist/first-package.umd.js", format: 'umd', name: 'first-package', sourcemap: true },
   ],
-  external: external(__dirname), // без него все падало на этапе билдежки
+  external: external(__dirname),
   plugins: [
     resolve({
       main: false,
--- packages/second-package/rollup.config.js
+++ packages/second-package/rollup.config.js
@@ -1,12 +1,11 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import { external, babelrc, globals, stubProcess } from '../../rollup-utils';
+import { external } from '../../rollup-utils';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: "dist/second-package.umd.js", format: 'umd', name: 'second-package', sourcemap: true, intro: stubProcess },
-    { file: "dist/second-package.es.js", format: 'es', sourcemap: true },
+    { file: "dist/second-package.umd.js", format: 'umd', name: 'second-package', sourcemap: true },
   ],
   external: external(__dirname),
   plugins: [
